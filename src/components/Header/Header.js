af5111d5fa756db2e0a20a782b3b3f87c69bb2a6
Carlos <carlos96831@gmail.com>
2022-01-19 21:12:09 -0800

    added links to LinkedIn, Instagram, Github, etc.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
@@ -32,14 +32,17 @@ const Header = () =>  (
       </li>        
     </Div2>
       <Div3>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://github.com/carvasquez206">
           <AiFillGithub size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://www.linkedin.com/in/carlosvasquez1/">
           <AiFillLinkedin size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://instagram.com">
           <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://youtube.com">
+          <AiFillYoutube size="3rem"/>
         </SocialIcons>
       </Div3>
     </Container>
