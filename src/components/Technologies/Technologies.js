4508e8a46004c50e7182dc40ef54a967bf8375cd
Carlos <carlos96831@gmail.com>
2022-01-20 22:29:23 -0800

    added description of tehcnologies and changed the icons

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiZend, } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <Section id="tech">
     <SectionDivider />
+    <br/>
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with Front-End and Back-End. I also have experience working as a QA Analyst and Regression Engineer for a video streaming service platform in the Greater Seattle area.
     </SectionText>
     <List>
       <ListItem>
-        <DiFirebase size="3rem" />
+        <DiReact size="3rem" />
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
@@ -32,9 +33,9 @@ const Technologies = () =>  (
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiFirebase size="3rem" />
+        <DiZend size="3rem" />
         <ListContainer>
-          <ListTitle>Software Quality Assurance</ListTitle>
+          <ListTitle>Software QA</ListTitle>
           <ListParagraph>
           Experience with  <br/>
             Smoke and Manual Regression Testing
