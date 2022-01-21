71bf9ee6525506a316bbe1229a2b1e8e629c2f70
Carlos <carlos96831@gmail.com>
2022-01-20 22:18:22 -0800

    added some description to the technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -8,8 +8,40 @@ const Technologies = () =>  (
     <SectionDivider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      hfjghffkhfkhg
+      I've worked with Front-End and Back-End. I also have experience working as a QA Analyst and Regression Engineer for a video streaming service platform in the Greater Seattle area.
     </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+          Experience with  <br/>
+            HTML, CSS, JS, and ReactJS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with  <br/>
+            NodeJS and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Software Quality Assurance</ListTitle>
+          <ListParagraph>
+          Experience with  <br/>
+            Smoke and Manual Regression Testing
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 );
 
